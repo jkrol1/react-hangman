7ad3cc051fc71fb884ce59b0bfb15859dcbb7b3e
Jan Król <jk.jankrol@gmail.com>
2020-05-23 16:39:30 +0200

    Updates

---
+++ precache-manifest.abef15b27da4e9eeb0603d5260391d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7829108fa047a67be8ce5e54bb650504",
+    "url": "/react-hangman/index.html"
+  },
+  {
+    "revision": "2a89ff782142123ea996",
+    "url": "/react-hangman/static/css/main.75890794.chunk.css"
+  },
+  {
+    "revision": "e511f28edf1e9be41a87",
+    "url": "/react-hangman/static/js/2.02aeddbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hangman/static/js/2.02aeddbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a89ff782142123ea996",
+    "url": "/react-hangman/static/js/main.0343974e.chunk.js"
+  },
+  {
+    "revision": "0f89436566966736c66c",
+    "url": "/react-hangman/static/js/runtime-main.4404f577.js"
+  }
+]);
